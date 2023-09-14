77ec02d907c0f4ca0893c7595fa5da0264e3e9c9
Vishnu <vishnunair20012112@gmail.com>
2023-09-14 15:40:24 +0530

    navigation working fine

--- lib/Lists/scrollview.dart
+++ lib/Lists/scrollview.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:basicversion/Lists/Secondartlist.dart';
+import 'package:basicversion/MainPage/mainpage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -23,19 +25,42 @@ class WidgetScrollerState extends State<WidgetScroller>{
     // TODO: implement build
     return   Container(color:Colors.orange,height:MediaQuery.of(context).size.height/21,
                           child:
-                             Obx(()=>
+                             Row(
+                               children: [
+                                 Expanded(
+                                   child: Obx(()=>
 
-                                  ListView.builder( scrollDirection: Axis.horizontal,
-                                                   itemCount: controller.directorylist.length,
-                                                   itemBuilder: ( context, index)=>
+                                        ListView.builder( scrollDirection: Axis.horizontal,
+                                                         itemCount: controller.directorylist.length,
+                                                         itemBuilder: ( context, index)=>
 
-                                           TextButton(onPressed: (){},
-                                                         child:
-                                                           Text(index==0?"Internal Storage "
-                                                                               :"${controller.directorylist[index]} >"),
-                                           ),
-                                          ),
-                                        ),
+                                                 TextButton(onPressed: (){
+                                                   if(index==controller.directorylist.length-1)
+                                                   {
+                                                   }
+                                                   else if(index==0){
+                                                     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)
+                                                                                       =>PrimaryPage()));
+                                                   }
+                                                   else{
+                                                     controller.directorylist.removeRange(index+1,controller.directorylist.length);
+                                                     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)
+                                                                                              =>SecondaryFileList()));
+                                                     print(2);
+                                                   }
+
+                                                 },
+                                                               child:
+                                                                 Text(index==0?"Internal Storage >"
+                                                                                     :"${controller.directorylist[index]} >"),
+                                                 ),
+                                                ),
+                                              ),
+                                 ),
+                                 Icon(Icons.grid_view_sharp),
+                                 Icon(Icons.more_vert),
+                               ],
+                             ),
 
 
 
